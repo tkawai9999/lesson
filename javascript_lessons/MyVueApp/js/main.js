4b6fce196727af3ff45edab5260174983c2dbb55
tkawai9999 <tkawai9999@gmail.com>
2019-09-07 22:05:19 +0900

    update

--- javascript_lessons/MyVueApp/js/main.js
+++ javascript_lessons/MyVueApp/js/main.js
@@ -1,13 +1,56 @@
 (function() {
-  "use strict";
+  'use strict';
 
-  var vm = new Vue({
-    el: "#app",
+  // Components
+  // 14 Componentに機能をもたせよう
+
+  var likeComponent = Vue.extend({
+    // 16 Propsで値を渡してみよう
+    // props: ['message'],
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+
+    // template: '<button>Like</button>'
+    // template: '<div><button>Like</button><button>Like</button></div>'
+    // 15 いいね！ボタンを実装してみよう
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    // template: '<button @click="countUp">Like {{ count }}</button>',
+    // 16 Propsで値を渡してみよう
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+        // 17 $emitを使ってイベントを発火させよう
+        this.$emit('increment');
+      }
+    }
+
+  
+  });
+
+
+  var app = new Vue({
+    el: '#app',
+    components: {
+      'like-component': likeComponent
+    },
+    // 17 $emitを使ってイベントを発火させよう
     data: {
-      //   02 Vue.jsの基本をおさえよう
-      //   name: "taguchi"
-      //   03 ToDoアプリを作ってみよう
-      todos: ["task 1", "task 2", "task 3"]
+      total: 0
+    },
+    methods: {
+      incrementTotal: function() {
+        this.total++;
+      }
     }
   });
+
 })();
